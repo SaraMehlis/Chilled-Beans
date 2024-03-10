08e91941939d7430f75f5f5f97fa24ec4ee62a42
Sara mehlis <Sara.mehlis@gmail.com>
2024-03-10 21:34:58 +0000

    Add class recipe to models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, max_length=150)
+    ingredient = models.TextField()
+    instruction = models.TextField()
+    nutrition_information = models.TextField()
+    image = CloudinaryField('image', default='placeholder')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
+def __str__(self):
+        return self.title
