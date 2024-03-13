c5c40496612ab28e7e82136adefe0e4eee2aa268
Sara mehlis <Sara.mehlis@gmail.com>
2024-03-13 16:06:46 +0000

    Install allauth and update base.html with authentication urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from .views import RecipeListView
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', RecipeListView.as_view(), name='index'),
     path('about/', views.about, name='about'),
     path('recipe/<slug:slug>/', views.recipe_detail, name='recipe_detail'),
+    path("accounts/", include("allauth.urls")),
 ]
--- my_project/settings.py
+++ my_project/settings.py
@@ -32,11 +32,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary',
     #'cloudinary_storage',
     #'django_summernote',
     'blog',
 ]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -47,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'my_project.urls'
@@ -102,6 +111,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 
 # Internationalization
