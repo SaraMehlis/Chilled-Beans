f4c3b88fcb006bea5b86b24f129cb7d5bf0d7144
Sara mehlis <Sara.mehlis@gmail.com>
2024-03-13 14:59:51 +0000

    Add recipe_detail.html and create if statement to allow authenticated user to edit and delete their recipes

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', RecipeListView.as_view(), name='index'),
     path('about/', views.about, name='about'),
-    path('<slug:slug>/', views.recipe_detail, name='recipe_detail'),
+    path('recipe/<slug:slug>/', views.recipe_detail, name='recipe_detail'),
 ]
